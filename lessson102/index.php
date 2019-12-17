0d3a776fe8fa931220bc95d24d0f36743f907332
hodoor6 <hodoor6@yandex.ua>
2019-12-17 22:15:16 +0200

    // Урок 101 задание 2 Получите массив с роутами в файле index.php.

---
+++ lessson102/index.php
@@ -0,0 +1,22 @@
+<?php
+// Урок 101 задание 1Создайте файл index.php и добавьте в него указанные строки.
+namespace Core;
+
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+
+// Урок 101 задание 3 Реализуйте функцию автозагрузки. Проверьте ее работу.
+spl_autoload_register(function($class) {
+
+   $filename = str_replace('\\', '/', $class );
+   //return $filename;
+});
+
+// Прочитываем массив из файла с роутами в переменную:
+$routes = require $_SERVER['DOCUMENT_ROOT'] . '/project/config/routes.php';
+
+// Урок 101 задание 2 Получите массив с роутами в файле index.php.
+var_dump($routes);
+
+?>
+
