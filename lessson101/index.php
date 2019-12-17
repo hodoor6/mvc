f92007701ee520284d2e56b7c15c7e0617473d90
hodoor6 <hodoor6@yandex.ua>
2019-12-17 22:06:31 +0200

    // Урок 101 задание 3 Реализуйте функцию автозагрузки. Проверьте ее работу.

--- lessson101/index.php
+++ lessson101/index.php
@@ -5,6 +5,11 @@ namespace Core;
 error_reporting(E_ALL);
 ini_set('display_errors', 'on');
 
-
+// Урок 101 задание 3 Реализуйте функцию автозагрузки. Проверьте ее работу.
+spl_autoload_register(function($class) {
+    $ds = DIRECTORY_SEPARATOR;
+    $filename = str_replace('\\',  $ds, $class ) . '.php';
+   require($filename);
+});
 ?>
 
