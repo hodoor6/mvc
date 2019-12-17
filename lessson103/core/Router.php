07774477ba851fcaccf4fd47f1dc00bc384d0e18
hodoor6 <hodoor6@yandex.ua>
2019-12-17 22:29:29 +0200

    //Урок 103 задание 2 Скопируйте мою заготовку класса Router и разместите его в файле /core/Router.php.

--- lessson103/core/Router.php
+++ lessson103/core/Router.php
@@ -1,1 +1,12 @@
 <?php
+//Урок 103 задание 2 Скопируйте мою заготовку класса Router и разместите его в файле /core/Router.php.
+class Router
+{
+    private $routes;
+
+    public function getTrack($routes, $uri)
+    {
+        // тут будет код
+
+    }
+}
